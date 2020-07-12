c1ef3d3b929ce556ac8e29cf2fe4a7a15ee5147a
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-07-12 12:38:07 -0400

    Rotate raster

--- Precipitation.R
+++ Precipitation.R
@@ -2,54 +2,67 @@ library(sf)
 library(raster)
 library(dplyr)
 library(ggplot2)
-#library(ncdf4)
 
+options(scipen=10)
+
+#Load Shapefiles from Chilean regions
 regiones <- read_sf("C:/Richard/R and Python/Environmental Data Science/Regiones/Regional.shp")
+
+#Load Rasters
+prec <- brick("C:/Richard/R and Python/Environmental Data Science/GIS/ChileClimate/Data_raw/precip.2019.nc")
+prec <- rotate(prec)
+
+#For Valparaiso remove Easter Island and Isla Juan Fernandez, then buffer to make it larger
 valpo <- read_sf("C:/Richard/R and Python/Environmental Data Science/Comunas/comunas.shp") %>% 
   filter(!Comuna %in% c("Isla de Pascua","Juan Fernández"),Region=="Región de Valparaíso") %>% 
   st_simplify(dTolerance=500) %>% 
-  st_buffer(400) %>% 
+  st_buffer(5000) %>% 
   st_union()
+  
 
-#chile <- comunas %>% st_union()
-
+#Plot Valparaiso Region without Easter Island
 valpo %>% 
 ggplot()+
   geom_sf()+
   theme(legend.position = "none")
 
+#Intersect Valparaiso with the new Valpo without Easter Island
 test <- regiones %>% filter(Region=="Región de Valparaíso") %>% 
+  st_buffer(dist=0) %>% 
   st_intersection(valpo)
 
-regiones <- regiones %>% filter(!Region%in%c("Región de Valparaíso","Zona sin demarcar")) %>%
+#Remove old Valparaiso region from regions and add new Valpo
+regiones <- regiones %>% 
+  filter(!Region%in%c("Región de Valparaíso","Zona sin demarcar")) %>%
   rbind(test)
 
+#Simplified version for faster visualization
 reg_simp <- regiones %>% 
   st_simplify(dTolerance=8000)
-
-reg_buff <- reg_simp %>% 
-  st_buffer(20000)
 
 reg_simp %>% 
   ggplot()+
   geom_sf()+
   theme(legend.position = "none")
 
+regiones <- regiones %>% st_transform(crs(prec)@projargs)
+reg_simp <- reg_simp %>% st_transform(crs(prec)@projargs)
+
 #Get the raster data
 
+#Change extent and CRS 
+#extent(prec) <- extent(-9000000,9000000,-18000000,18000000)
+crs(prec) <- crs(regiones)
 
-ncin <- brick("C:/Richard/R and Python/Environmental Data Science/GIS/ChileClimate/Data_raw/precip.2019.nc")
-ncin <- brick("C:/Richard/R and Python/Environmental Data Science/GIS/ChileClimate/Data_raw/precip.mon.mean.nc")
-#ncin <- projectRaster(ncin, crs = crs("+init=epsg:3857"))
-extent(ncin) <- extent(-18000000,18000000,-9000000,9000000)
 
-crs(ncin) <- crs(regiones)
-test <- raster::intersect(x=ncin[[1]],y=reg_buff)
+rt <- data.frame(rasterToPoints(mask(prec[[161]],reg_simp)))
 
-rt <- data.frame(rasterToPoints(mask(test,reg_buff)))
 names(rt) <- c("x","y","value")
 
 
+
 ggplot()+
-  geom_raster(data=rt,aes(x=x,y=y,fill=value))+
+  geom_tile(data=rt,aes(x=x,y=y,fill=value))+
   geom_sf(data=reg_simp,col="red",fill=NA)
+
+
